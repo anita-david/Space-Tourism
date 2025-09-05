99fdfddc9a5a303d3657b3daa8e514cd8430f736
Anita David <anitafunmilayo@gmail.com>
2025-09-05 12:07:43 +0100

    Polished the navigation menu for responsiveness

--- src/components/header/NavMenu.jsx
+++ src/components/header/NavMenu.jsx
@@ -1,10 +1,8 @@
 import { NavLink } from "react-router-dom";
 
 function NavMenu({ direction = "row" }) {
-  const baseClasses =
-    "text-white font-normal uppercase";
-  const itemClasses =
-    "flex items-center gap-2";
+  const baseClasses = "text-white font-normal uppercase";
+  const itemClasses = "flex items-center gap-2";
 
   return (
     <div
@@ -13,31 +11,75 @@ function NavMenu({ direction = "row" }) {
           ? // Desktop layout
             "hidden sm:flex lg:flex justify-between gap-[48px] px-[50px] bg-white/5 p-[40px] sm:text-[16px] lg:text-[18px]"
           : // Mobile sidebar layout
-            "flex flex-col gap-6 text-[18px] "
+            "flex flex-col gap-6 text-[18px]"
       }
     >
-      <NavLink to="/">
-        <button className={`${baseClasses} ${itemClasses}`}>
-          <span className="font-bold px-[5px]">00</span> Home
-        </button>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          `${baseClasses} ${itemClasses} ${
+            direction === "row"
+              ? isActive
+                ? "border-b-[3px] border-white pb-2"
+                : "border-b-[3px] border-transparent pb-2"
+              : isActive
+              ? "border-r-[3px] border-white pr-2"
+              : "border-r-[3px] border-transparent pr-2"
+          }`
+        }
+      >
+        <span className="font-bold px-[5px]">00</span> Home
       </NavLink>
 
-      <NavLink to="/destination">
-        <button className={`${baseClasses} ${itemClasses}`}>
-          <span className="font-bold px-[5px]">01</span> Destination
-        </button>
+      <NavLink
+        to="/destination"
+        className={({ isActive }) =>
+          `${baseClasses} ${itemClasses} ${
+            direction === "row"
+              ? isActive
+                ? "border-b-[3px] border-white pb-2"
+                : "border-b-[3px] border-transparent pb-2"
+              : isActive
+              ? "border-r-[3px] border-white pr-2"
+              : "border-r-[3px] border-transparent pr-2"
+          }`
+        }
+      >
+        <span className="font-bold px-[5px]">01</span> Destination
       </NavLink>
 
-      <NavLink to="/crew">
-        <button className={`${baseClasses} ${itemClasses}`}>
-          <span className="font-bold px-[5px]">02</span> Crew
-        </button>
+      <NavLink
+        to="/crew"
+        className={({ isActive }) =>
+          `${baseClasses} ${itemClasses} ${
+            direction === "row"
+              ? isActive
+                ? "border-b-[3px] border-white pb-2"
+                : "border-b-[3px] border-transparent pb-2"
+              : isActive
+              ? "border-r-[3px] border-white pr-2"
+              : "border-r-[3px] border-transparent pr-2"
+          }`
+        }
+      >
+        <span className="font-bold px-[5px]">02</span> Crew
       </NavLink>
 
-      <NavLink to="/technology">
-        <button className={`${baseClasses} ${itemClasses}`}>
-          <span className="font-bold px-[5px]">03</span> Technology
-        </button>
+      <NavLink
+        to="/technology"
+        className={({ isActive }) =>
+          `${baseClasses} ${itemClasses} ${
+            direction === "row"
+              ? isActive
+                ? "border-b-[3px] border-white pb-2"
+                : "border-b-[3px] border-transparent pb-2"
+              : isActive
+              ? "border-r-[3px] border-white pr-2"
+              : "border-r-[3px] border-transparent pr-2"
+          }`
+        }
+      >
+        <span className="font-bold px-[5px]">03</span> Technology
       </NavLink>
     </div>
   );
